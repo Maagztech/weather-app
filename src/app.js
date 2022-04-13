1c8bf8a81692f7bee341eb059f2c3b3be6462426
Asutosh Pradhan <20ec3014@rgipt.ac.in>
2022-04-13 11:43:26 +0530

    App is working properly

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-const geocode = require('./utils/geocode')
+
 const forecast = require('./utils/forecast')
 
 const app = express()
@@ -47,22 +47,16 @@ app.get('/weather', (req, res) => {
             error: 'You must provide an address!'
         })
     }
+    let address = req.query.address
 
-    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    forecast(address, (error, forecastData) => {
         if (error) {
             return res.send({ error })
         }
 
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                return res.send({ error })
-            }
-
-            res.send({
-                forecast: forecastData,
-                location,
-                address: req.query.address
-            })
+        res.send({
+            forecast: forecastData,
+            address: req.query.address,
         })
     })
 })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,14 +1,14 @@
 const request = require('request')
 
-const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current? access_key = 1dda044300e1fa1762684cb418e5f341& query = New York'
+const forecast = (address, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=1dda044300e1fa1762684cb418e5f341&query='+address
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined,  ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
         }
     })
 }
